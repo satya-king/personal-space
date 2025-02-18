1bad573ed16c79ed061e317ccc805cbf0c11f13a
satya <satyanarayana.p@apcfss.in>
2025-02-18 12:23:51 +0530

    dev

--- src/APIURLs/Urls.js
+++ src/APIURLs/Urls.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8080/learn/login"
+export const BASE_URL = "https://learn-spring-boot.onrender.com/learn/login"
+// export const BASE_URL = "http://localhost:8080/learn/login"
