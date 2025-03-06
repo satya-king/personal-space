55dad36423912a58de27c548bba11716b4d7e78e
satya <satyanarayana.p@apcfss.in>
2025-03-06 15:13:24 +0530

    c

--- src/APIURLs/Urls.js
+++ src/APIURLs/Urls.js
@@ -1,2 +1,7 @@
 export const BASE_URL = "https://learn-spring-boot.onrender.com/learn/login"
 // export const BASE_URL = "http://localhost:8080/learn/login"
+
+
+
+
+LOGIN_URL = "https://personal-space-lime.vercel.app/"
