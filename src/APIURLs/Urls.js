be404d67903a9964bf54f1ed59d02fd741f6d414
satya <satyanarayana.p@apcfss.in>
2026-04-23 16:46:20 +0530

    user

--- src/APIURLs/Urls.js
+++ src/APIURLs/Urls.js
@@ -8,5 +8,7 @@ export const LOGIN_URL = "https://personal-space-lime.vercel.app/"
 
 
 
+
+
 // APIKEY for payment gateway
 export const vercel_AI_GATEWAY = "vck_0pBFguGwmJD0qDl1iuwem3F4IXmdRvVkWzjeZo9Z3E4xPfRVD72sx8ms";
